5ff2c45e80f2367323314a07c291c19702fb6a04
Robert Kan <106627599+robkkan@users.noreply.github.com>
2024-09-01 23:05:07 -0500

    Update globe.js

--- globe.js
+++ globe.js
@@ -1,58 +1,45 @@
 a1111globe();
 
-function a1111globe()
-{
-        let id = "a1111globe";
+function a1111globe() {
+    let id = "a1111globe";
 
-
-        let scriptAnimArr = [
+    let scriptAnimArr = [
         'https://robkkan.github.io/portfolio/globe_cloud.js',
-        'https://robkkan.github.io/portfolio/globe_gsap.js',
-        ]
+        'https://robkkan.github.io/portfolio/globe_gsap.js'
+    ];
 
-        function gsapAnim()
-        {
-            gsap.registerPlugin(MorphSVGPlugin); 
+    function gsapAnim() {
+        gsap.registerPlugin(MorphSVGPlugin);
+        gsap.set(id, { opacity: 1 });
+        gsap.set(".main_animation_container svg", { opacity: 1 });
 
-            gsap.set(id, { opacity: 1 })
+        gsap.to('#globeL1', {
+            morphSVG: '#globeR1',
+            duration: 6,
+            repeat: -1,
+            ease: "linear"
+        }, "-10");
+        gsap.to('#globeL2', {
+            morphSVG: '#globeR2',
+            duration: 6,
+            repeat: -1,
+            ease: "linear"
+        }, "<+4");
+        gsap.to('#globeL3', {
+            morphSVG: '#globeR3',
+            duration: 6,
+            repeat: -1,
+            ease: "linear"
+        }, "<+4");
+        gsap.to('#globeL4', {
+            morphSVG: '#globeR4',
+            duration: 6,
+            repeat: -1,
+            ease: "linear"
+        }, "<+4");
+    }
 
-        gsap.set(".main_animation_container svg", {opacity: 1})
-                    const tlglobe2 = gsap.timeline({ repeat: -1, delay: 0.4,  defaults: { ease: "linear" } })
-
-
-                    gsap.to('#globeL1', {
-                        morphSVG: '#globeR1',
-                        duration: 6,
-                        repeat: -1,
-                        ease: "linear" 
-                    },"-10")
-                    gsap.to('#globeL2', {
-                        morphSVG: '#globeR2',
-                        duration: 6, 
-                        repeat: -1,
-                        ease: "linear" 
-                    }, "<+4")
-                    gsap.to('#globeL3', {
-                        morphSVG: '#globeR3',
-                        duration: 6,
-                        repeat: -1,
-                        ease: "linear" 
-                    }, "<+4")
-                    gsap.to('#globeL4', {
-                        morphSVG: '#globeR4',
-                        duration: 6,
-                        repeat: -1,
-                        ease: "linear" 
-                    }, "<+4")
-
-        }
-
-
-
-
-        let contents =
-        ` 
-
+    let contents = ` 
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 width="258px" height="258px" viewBox="0 0 258 258" enable-background="new 0 0 258 258" xml:space="preserve">
 <g>
@@ -98,57 +85,52 @@ width="258px" height="258px" viewBox="0 0 258 258" enable-background="new 0 0 25
 </g>
 </svg>       
         
-        `
-        ;
+ `;
 
+    let animWrapper = document.getElementById(id);
+    let fullId = "#" + id + " ";
 
-        let animWrapper = document.getElementById(id);
-        id = "#"+id+" ";
+    function importSVG() {
+        animWrapper.innerHTML = contents;
+        document.querySelector(".animation_wrapper svg").style.width = "100%";
+        document.querySelector(".animation_wrapper svg").style.height = "100%";
+        window.addEventListener("load", gsapAnim);
+    }
 
+    function addScripts() {
+        let finalAddList = [];
+        let scriptList;
 
-        function importSVG()
-        {
-            animWrapper.innerHTML = contents;
-            document.querySelector(".animation_wrapper svg").style.width = "100%";
-            document.querySelector(".animation_wrapper svg").style.height = "100%";
-            
-            window.addEventListener("load", gsapAnim)
-        }
-
-        function addScripts()
-        {
-            let finalAddList = [];
-
-
-            let scriptList;
-            if(document.body != null && document.getElementsByClassName('containerScriptList').length == 0){
-                scriptList = document.createElement('div');
-                scriptList.classList.add('containerScriptList')
-                document.body.appendChild(scriptList);
-                finalAddList = scriptAnimArr;
+        if (document.body != null && document.getElementsByClassName('containerScriptList').length == 0) {
+            scriptList = document.createElement('div');
+            scriptList.classList.add('containerScriptList');
+            document.body.appendChild(scriptList);
+            finalAddList = scriptAnimArr;
+        } else {
+            scriptList = document.getElementsByClassName('containerScriptList')[0];
+            let existArr = [];
+            let existScriptsLinks = document.getElementsByClassName('containerScriptList')[0].getElementsByTagName("script");
+            for (let i = 0; i < existScriptsLinks.length; i++) {
+                existArr.push(existScriptsLinks[i].src);
             }
-            else
-            {
-                scriptList = document.getElementsByClassName('containerScriptList')[0];
-                let existArr = [];
-                let existScriptsLinks =  document.getElementsByClassName('containerScriptList')[0].getElementsByTagName("script");
-                for (let i = 0; i < existScriptsLinks.length; i++) {
-                    existArr.push(existScriptsLinks[i].src);
+            scriptAnimArr.forEach((elem) => {
+                if (existArr.indexOf(elem) == -1) {
+                    finalAddList.push(elem);
                 }
-                scriptAnimArr.forEach((elem)=>{
-                    if(existArr.indexOf(elem) == -1)
-                    {
-                        finalAddList.push(elem);
-                    }
-                })
-            }
-            finalAddList.forEach((elem)=>{
-                let tempScript = document.createElement('script')
-                tempScript.src = elem
-                scriptList.appendChild(tempScript);
-            })
+            });
         }
+        finalAddList.forEach((elem) => {
+            let tempScript = document.createElement('script');
+            tempScript.src = elem;
+            tempScript.onload = function() {
+                if (elem.includes('globe_gsap.js')) {
+                    gsapAnim(); // Call after the necessary scripts are loaded
+                }
+            };
+            scriptList.appendChild(tempScript);
+        });
+    }
 
-        addScripts();
-        importSVG();
+    addScripts();
+    importSVG();
 }
